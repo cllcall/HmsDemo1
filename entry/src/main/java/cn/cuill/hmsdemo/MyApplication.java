73411e9c1943f8ae5238d0f61328e601a6bb62e0
cllcall <2236445873@qq.com>
2021-05-23 11:02:01 +0800

    Initial commit

---
+++ entry/src/main/java/cn/cuill/hmsdemo/MyApplication.java
@@ -0,0 +1,10 @@
+package cn.cuill.hmsdemo;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/cn/cuill/hmsdemo/ExampleTest.java
@@ -0,0 +1,9 @@
+package cn.cuill.hmsdemo;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
